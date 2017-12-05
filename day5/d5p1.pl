89acb9274549641030ee771409fceffaf2b1757b
ngyj <ngyj@cinnabar.fr>
2017-12-05 18:15:21 +0100

    day 5

---
+++ day5/d5p1.pl
@@ -0,0 +1,14 @@
+#!/bin/perl
+
+use strict;
+use warnings;
+use 5.022;
+
+my @instructions = <>;
+my $steps = 0;
+my $i=0;
+while($i < scalar(@instructions)) {
+	$i += $instructions[$i]++;
+	$steps++;
+}
+say $steps;
