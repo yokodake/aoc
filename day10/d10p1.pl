cf10baccfa0e43fd22251e4276f4cfaea7dc9d37
namigyj <ngyj@cinnabar.fr>
2017-12-10 23:04:39 +0100

    day10

--- day10/d10p1.pl
+++ day10/d10p1.pl
@@ -5,33 +5,53 @@ use warnings;
 use v5.22;
 
 my @list;
-my @size_list = ( 3, 4, 1, 5 );
+my @size_list;
 my $idx = 0;
 my $skip = 0;
 my $length;
 
-@list =  make_list(10);
+if($ARGV[0] eq 'test') { @list = make_list(5); @size_list = ( 3, 4, 1, 5 ); }
+else { @list = make_list(256); @size_list = split ',', <>;}
 
-say @nlist;
+say round();
 
-# returns the reverse list
-sub rv {
-	my @rlist;
-
-
+sub round {
+	for(@size_list) {
+		$length = $_;
+		# debug();
+		ins($idx, $length, reverse ext($idx, $length, @list));
+		# say @list;
+		$idx = ($idx +($length + $skip++))% @list;
+	}
+	return ($list[0] * $list[1]);
 }
+
 # extract : return the sub-list of length
 sub ext {
+	my $i = shift;
+	my $len = shift;
+	my @olist = @_;
 	my @nlist;
-	for(0..$length){
-		push @nilst, $list[$idx % @list];
+	for(0..($len-1)){
+		push @nlist, $olist[($i+$_) % @olist];
 	}
 	return @nlist;
 }
-
+# insert : inserts the new list inside it
+sub ins {
+	my $i = shift;
+	my $len = shift;
+	my @nlist = @_;
+	for(0..($len-1)){
+		$list[($i+$_) % @list] = $nlist[$_];
+	}
+}
 sub make_list {
 	my $size = (shift) - 1;
 	my @list;
 	push @list, $_ for(0..$size);
 	return @list;
+}
+sub debug {
+	print "i:$idx l:$length s:$skip  ";
 }
--- day10/d10p2.pl
+++ day10/d10p2.pl
@@ -3,3 +3,97 @@
 use strict;
 use warnings;
 use v5.22;
+
+my @list;
+my @len_list;
+
+my %tests = ( '' => 'a2582a3a0e66e6e86e3812dcb672a272',
+		   'AoC 2017' => '33efeb34ea91902bb2f59c9920caa6cd',
+		   '1,2,3' => '3efbe78a8d82f29979031a4aa0b16a9d', 
+		   '1,2,4' => '63960835bcdc130f0b66d7ff4f6a5a8e');
+
+if($ARGV[0] eq 'test') { 
+	for(keys %tests) {
+		my $result = main($_);
+		say "\"$_\" ($result)" if($result ne $tests{$_});
+	}
+} else { 
+	say main(<>);
+}
+
+
+sub main {
+	my $idx = 0;
+	my $skip = 0;
+	my $input = shift;
+	@list = make_list(256);
+	@len_list = make_len($input);
+
+	($idx, $skip) = round($idx, $skip) for(0..63);
+	my @hash = densify(@list);
+
+	return sprintf "%02x" x @hash, @hash;
+}
+sub round {
+	my $idx = shift;
+	my $skip = shift;
+	my $length;
+
+	for(@len_list) {
+		$length = $_;
+		my @reversed = reverse ext($idx, $length, @list);
+		for(0..($length-1)) {
+			$list[($idx+$_) % @list] = $reversed[$_];
+		}
+		$idx = ($idx +($length + $skip++))% @list;
+	}
+	return ($idx, $skip);
+}
+sub densify {
+	my @sparse_hash = @_;
+	die "block from sparse hash is not 16 bytes" if (@sparse_hash != 256);
+	
+	my @dense_hash;
+	for (0..15) {
+		push @dense_hash, reduce(ext($_*16, 16, @sparse_hash));
+	}
+	return @dense_hash;
+
+	sub reduce {
+		my @block = @_;
+		die "block from sparse hash is not 16 bytes" if (@block != 16);
+
+		my $dense = 0;
+		for(@block) {
+			$dense ^= $_;
+		}
+		return $dense;
+	}
+}
+# extract : return the sub-list of length
+sub ext {
+	my $i = shift;
+	my $len = shift;
+	my @olist = @_;
+	my @nlist;
+	for(0..($len-1)){
+		push @nlist, $olist[($i+$_) % @olist];
+	}
+	return @nlist;
+}
+# insert : inserts the new list inside it
+sub make_list {
+	my $size = (shift) - 1;
+	my @list;
+	push @list, $_ for(0..$size);
+	return @list;
+}
+sub make_len {
+	my $string = shift;
+	my @len_list;
+	for(split //, $string) {
+		push @len_list, ord($_);
+	}
+	push @len_list, (17,31,73,47,23);
+	return @len_list;
+}
